89d5b2f979beef839097a77c5ba6820801110db2
dysthmia <maksim.fediarin@mail.ru>
2026-02-14 16:54:12 +0300

    update Program.cs

--- Lab7/Program.cs
+++ Lab7/Program.cs
@@ -1,10 +1,98 @@
-﻿namespace Lab7
+namespace Lab7
 {
     public class Program
     {
         public static void Main()
         {
-           
+            // var s1 = new Purple.Task4.Sportsman("Maksim","Fediarin");
+            // var s2 = new Purple.Task4.Sportsman("David","Kazadze");
+            // var s3 = new Purple.Task4.Sportsman("Egor","Rychov");
+            // var s4 = new Purple.Task4.Sportsman("Maria","Wensday");
+            // var s5 = new Purple.Task4.Sportsman("Sasha","Igorevna");
+            // var s6 = new Purple.Task4.Sportsman("Misha","Blockbaster");
+
+            // s1.Run(17.32);
+            // s2.Run(34.3);
+            // s3.Run(5.890);
+            // s4.Run(19.89);
+            // s5.Run(34.3);
+            // s6.Run(15.27);
+
+            // var g1 = new Purple.Task4.Group("BIVT_25_9");
+            // var g2 = new Purple.Task4.Group("QWERTY");
+            // var g3 = new Purple.Task4.Group("Zxc CURSED");
+
+            // var spotsmen1 = new Purple.Task4.Sportsman[] {s1,s2};
+            // var spotsmen2 = new Purple.Task4.Sportsman[] {s1,s2,s3};
+            // var spotsmen3 = new Purple.Task4.Sportsman[] {s5,s6};
+            // var spotsmen4 = new Purple.Task4.Sportsman[] {s4,s5,s6};
+
+            // g1.Add(s1);
+            // g1.Add(spotsmen4);
+            // var g4 = new Purple.Task4.Group(g1);
+            // g2.Add(spotsmen3);
+            // g4.Add(g2);
+            
+            
+            // g1.Sort();
+            // task 3 for SetPlaces
+            // var p1 = new Purple.Task3.Participant("Maksim","Fediarin");
+            // var p2 = new Purple.Task3.Participant("Egor","Rychov");
+            // var p3 = new Purple.Task3.Participant("Fedor","Emelianenko");
+            // var p4 = new Purple.Task3.Participant("Brazil","Brazil");
+            // var p5 = new Purple.Task3.Participant("Lengend","ZXC");
+            // var p6 = new Purple.Task3.Participant("Brotha","Saluki");
+            // var p7 = new Purple.Task3.Participant("Makan","Makar");
+            // var p8 = new Purple.Task3.Participant("Beruza","Igorevna");
+
+            // double[] m1 = { 1.2, 5.8, 3.4, 0.6, 4.9, 2.1, 5.0 };
+            // double[] m2 = { 6.0, 2.7, 1.5, 3.3, 4.4, 0.2, 5.6 };
+            // double[] m3 = { 3.9, 4.1, 0.0, 2.8, 5.3, 1.7, 6.0 };
+            // double[] m4 = { 2.2, 3.6, 4.8, 1.1, 0.9, 5.5, 3.0 };
+            // double[] m5 = { 5.7, 0.4, 2.6, 6.0, 3.8, 1.9, 4.2 };
+            // double[] m6 = { 4.0, 1.3, 5.1, 2.4, 3.7, 0.8, 6.0 };
+            // double[] m7 = { 0.5, 2.9, 4.3, 5.4, 1.6, 3.1, 6.0 };
+            // double[] m8 = { 1.5, 3.9, 5.3, 1.4, 2.6, 4.1, 5.0 };
+
+            // // p1.Evaluate(m1);
+            // // p2.Evaluate(m2);
+            // // p3.Evaluate(m3);
+            // // p4.Evaluate(m4);
+            // // p5.Evaluate(m5);
+            // // p6.Evaluate(m6);
+            // // p7.Evaluate(m7);
+            // // p8.Evaluate(m8);
+
+            // var participant = new Purple.Task3.Participant[] {p1,p2,p3,p4,p5,p6,p7,p8};
+            // Purple.Task3.Participant.SetPlaces(participant);
+
+            // p1.Print();
+            // p2.Print();
+            // p3.Print();
+            // p4.Print();
+            // p5.Print();
+            // p6.Print();
+            // p7.Print();
+            // p8.Print();
+
+            // var r1 = new Purple.Task5.Response("Dog","","");
+            // var r2 = new Purple.Task5.Response("Dog","","");
+            // var r3 = new Purple.Task5.Response("Cat","","");
+            // var r4 = new Purple.Task5.Response("Cat","","");
+            // var r5 = new Purple.Task5.Response("Cat","","");
+            // var r6 = new Purple.Task5.Response("qwe","","");
+            // var r7 = new Purple.Task5.Response("albo","","");
+            // var r8 = new Purple.Task5.Response("mirele","","");
+            // var r9 = new Purple.Task5.Response("live","","");
+            // var r10 = new Purple.Task5.Response("live","","");
+
+            // // cat dog live qwe albo
+            // var response = new Purple.Task5.Response[]{r1,r2,r3,r4,r5,r6,r7,r8,r9,r10};
+            // var s = new Purple.Task5.Research("search",response);
+            // s.Print();
+            // var answer = s.GetTopResponses(1);
+            // System.Console.WriteLine("______answer________");
+            // System.Console.WriteLine(string.Join(" ",answer));
         }
     }
 }
